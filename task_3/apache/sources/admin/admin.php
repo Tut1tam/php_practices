bf4eca4b3dd32bb25917aab9ae37942388957b16
Tut1tam <dolzhich.maxim@yandex.ru>
2022-10-26 12:15:44 +0300

    final fixes

--- task_3/apache/sources/admin/admin.php
+++ task_3/apache/sources/admin/admin.php
@@ -1,39 +1,44 @@
-<!DOCTYPE html>
-
-<html>
+<!doctype html>
+<html lang="en">
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Main LIBRARY</title>
-        <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
-        <link href="default.css" rel="stylesheet" type="text/css" media="all"/>
-        <link href="fonts.css" rel="stylesheet" type="text/css" media="all"/>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Main library</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     </head>
     <body>
-    <div id="header-wrapper">
-        <div id="header" class="container">
-            <div id="logo">
-                <h1><span class="fa fa-bolt"></span><a href="#">городская библиотека</a></h1>
-            </div>
-            <div id="menu">
-                <ul>
-                    <li></li><a href="main.html">Главная</a></li>
-                    <li class="current_page_item"><a href="catalog.php">Каталог</a></li>
-                    <li><a href="about.html">О нас</a></li>
-                    <li><a href="libraries.php">Контакты</a></li>
-                    <li><a href="/admin/admin.php">Панель Администратора</a></li>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="../main.html">Городская библиотека</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="../main.html">Главная</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../catalog.php">Каталог</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../aboutus.html">О нас</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../libraries.php">Контакты</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/admin/admin.php">Панель Администратора<span class="sr-only">(current)</span></a>
+                </li>
                 </ul>
             </div>
-        </div>
-    </div>
+            </nav>
     <div id="header-featured">
         <div id="banner-wrapper">
         <?php
-            echo "<div>
-                Тут находиться панель администратора
-            </div>
-            ";            
+            echo "<span class='badge text-bg-primary'>Тут находиться панель администратора</span>";            
         ?>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     </body>
 </html>
